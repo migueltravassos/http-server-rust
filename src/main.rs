23d1e0bf310d6bfc296f3b6397ae90a1748a5df6
migueltravassos <miguel.ft1706@gmail.com>
2026-04-29 22:10:33 +0100

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use std::fs;
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
-use std::thread;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Couldnt bind");
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        thread::spawn(|| {
-            handle_connection(stream);
-        });
+        match stream {
+            Ok(t) => handle_connection(t),
+            Err(e) => eprintln!("Failed to establish connection {}.", e),
+        }
     }
 }
 
@@ -17,22 +15,30 @@ fn handle_connection(mut stream: TcpStream) {
     let reader = BufReader::new(&mut stream);
     let request: Vec<_> = reader
         .lines()
-        .map(|result| result.unwrap())
+        .filter_map(|result| match result {
+            Ok(r) => Some(r),
+            Err(e) => {
+                eprintln!("Error: {}", e);
+                None
+            }
+        })
         .take_while(|line| !line.is_empty())
         .collect();
 
-    let (status, filename) = match request[0].as_str() {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
-        "GET /about HTTP/1.1" => ("HTTP/1.1 200 OK", "about.html"),
-        "GET /admin HTTP/1.1" => ("HTTP/1.1 403 FORBIDDEN", "about.html"),
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    if request.is_empty() {
+        return;
+    }
+
+    let response = match request[0].as_str() {
+        "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\nHello",
+        "GET /about HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\nSoon",
+        _ => "HTTP/1.1 404 NOT FOUND\r\n\r\nNOT FOUND",
     };
-    let contents = fs::read_to_string(&filename).unwrap_or_default();
-    let response = format!(
-        "{}\r\nContent-Type: text/html\r\nContent-Length: {}\r\n\r\n{}",
-        status,
-        contents.len(),
-        contents
-    );
-    stream.write_all(response.as_bytes()).unwrap();
+
+    match stream.write_all(response.as_bytes()) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    }
 }
